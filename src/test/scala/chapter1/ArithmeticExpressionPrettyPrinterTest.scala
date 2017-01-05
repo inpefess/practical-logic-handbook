e1539f22a8af1d8109101e3831e76e1b3e7e745f
Boris Shminke <schminkeba@gmail.com>
2017-01-05 14:26:33 +0300

    made ArithmeticExpressionPrettyPrinter test more concise

--- src/test/scala/chapter1/ArithmeticExpressionPrettyPrinterTest.scala
+++ src/test/scala/chapter1/ArithmeticExpressionPrettyPrinterTest.scala
@@ -5,6 +5,6 @@ import org.scalatest.FunSuite
 class ArithmeticExpressionPrettyPrinterTest extends FunSuite {
   test("testPrettyPrint") {
     val input = "((1 + 2) * 3) ^ 4"
-    assert(ArithmeticExpressionPrettyPrinter.prettyPrint(ArithmeticExpressionParser.parse(input), 1) == input)
+    assert(ArithmeticExpressionParser.parse(input).mkString == input)
   }
 }
